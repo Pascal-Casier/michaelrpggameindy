12f05ae835b4529705282b5b29205424a0189f1c
Pascal Casier <pascal.casier@yahoo.com.br>
2025-06-02 10:44:37 -0300

    added tilemap

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -2,29 +2,30 @@ class_name Player extends CharacterBody2D
 
 var cardinal_direction := Vector2.DOWN
 var direction := Vector2.ZERO
-var move_speed := 100.0
-var state : String = "idle"
+
+
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var sprite: Sprite2D = %Sprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
 
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	state_machine.initialize(self)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
-	velocity = direction * move_speed
-	
-	if set_state() == true or set_direction() == true:
-		update_animation()
+	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
+
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
@@ -44,14 +45,9 @@ func set_direction() -> bool:
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 	
-func set_state() -> bool :
-	var new_state := "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
+
 	
-func update_animation() -> void:
+func update_animation(state : String) -> void:
 	animation_player.play(state + "_" + anim_direction())
 	pass
 	
