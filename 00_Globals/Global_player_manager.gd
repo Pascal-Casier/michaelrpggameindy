f4cb8df3fd2f03926e1542cf0fa4ede5664ced0a
Pascal Casier <pascal.casier@yahoo.com.br>
2025-06-08 10:59:45 -0300

    E13 added playerSpawner

--- 00_Globals/Global_player_manager.gd
+++ 00_Globals/Global_player_manager.gd
@@ -1,4 +1,28 @@
 extends Node
 
+const PLAYER = preload("res://Player/player.tscn")
 
 var player : Player
+var player_spawned := false
+
+func _ready() -> void:
+	add_player_instance()
+	
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child(player)
+
+func set_player_position(_new_pos : Vector2) -> void:
+	player.global_position = _new_pos
+	pass
+
+func set_as_parent(_p : Node2D) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+	_p.add_child(player)
+	
+func unparent_player(_p : Node2D) -> void:
+	_p.remove_child(player)
+	
+	
+	
