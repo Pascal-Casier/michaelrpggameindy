a10e73850c9c0ad1b4c45c061ec719d4d7c6510b
Pascal Casier <pascal.casier@yahoo.com.br>
2025-06-09 09:00:02 -0300

    E15 Save_load System

--- Levels/Scripts/level.gd
+++ Levels/Scripts/level.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent(self)
 	LevelManager.level_load_started.connect(_free_level)
+	
 
 func _free_level() -> void:
 	PlayerManager.unparent_player(self)
