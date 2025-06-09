768d6684b9f8e281c65584eb1c801ab7762a6cc5
Pascal-Casier <pascal.casier1973@gmail.com>
2025-06-09 17:16:40 -0300

    E17 pickup items + inventory part 2

--- Items/Scripts/item_data.gd
+++ Items/Scripts/item_data.gd
@@ -3,3 +3,14 @@ class_name ItemData extends Resource
 @export var name := ""
 @export_multiline var description := ""
 @export var texture : Texture2D 
+
+@export_category("Item use effect")
+@export var effects : Array[ItemEffect]
+
+func use() -> bool:
+	if effects.size() == 0:
+		return false
+	for e in effects:
+		if e:
+			e.use()
+	return true
