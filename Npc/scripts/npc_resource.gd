5761addb21d4a182e7ceb192463b486799130703
Pascal Casier <pascal.casier@yahoo.com.br>
2026-01-16 16:36:43 -0300

    E29 done : NPC part 1

---
+++ Npc/scripts/npc_resource.gd
@@ -0,0 +1,6 @@
+class_name NPCResource extends Resource
+
+@export var NPC_name : String = ""
+@export var sprite : Texture
+@export var portrait : Texture
+@export var dialogue_audio_pitch : String = ""
