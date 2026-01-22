c2c2fe096143bec4aa07e402a902d634044c3d01
Pascal Casier <pascal.casier@yahoo.com.br>
2026-01-22 15:30:49 -0300

    E30 minur bug fixes

--- Npc/scripts/npc_behavior_patrol.gd
+++ Npc/scripts/npc_behavior_patrol.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		child_entered_tree.connect(gather_patrol_locations)
 		child_order_changed.connect(gather_patrol_locations)
+		return
 	pass
 	super()
 	if patrol_locations.size() == 0:
