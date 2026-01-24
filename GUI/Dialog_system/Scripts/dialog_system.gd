2e068d61496637656150b13770e06ef12ca331c9
Pascal-Casier <pascal.casier1973@gmail.com>
2026-01-24 17:59:53 -0300

    E 35 done choice dialog

--- GUI/Dialog_system/Scripts/dialog_system.gd
+++ GUI/Dialog_system/Scripts/dialog_system.gd
@@ -7,6 +7,7 @@ signal letter_added(letter : String)
 
 var is_acive : bool = false
 var text_in_progress : bool = false
+var waitin_for_choice : bool = false
 
 var text_speed : float = 0.02
 var text_length : int = 0
@@ -23,6 +24,7 @@ var dialog_item_index : int = 0
 @onready var dialog_progress_indicator_label: Label = $DialogUI/DialogProgressIndicator/Label
 @onready var timer: Timer = $DialogUI/Timer
 @onready var audio_stream_player: AudioStreamPlayer = $DialogUI/AudioStreamPlayer
+@onready var choice_options: VBoxContainer = $DialogUI/VBoxContainer
 
 
 func _ready() -> void:
@@ -48,6 +50,8 @@ func _unhandled_input(event: InputEvent) -> void:
 			text_in_progress = false
 			show_dialog_button_indicator(true)
 			return
+		elif waitin_for_choice == true:
+			return
 		dialog_item_index += 1
 		if dialog_item_index < dialog_items.size():
 			start_dialog()
@@ -68,6 +72,7 @@ func show_dialog(_items : Array[DialogItem]) -> void:
 	
 func hide_dialog() -> void:
 	is_acive = false
+	choice_options.visible = false
 	dialog_ui.hide()
 	dialog_ui.process_mode = Node.PROCESS_MODE_DISABLED
 	get_tree().paused = false
@@ -75,15 +80,49 @@ func hide_dialog() -> void:
 	pass
 
 func start_dialog() -> void:
+	waitin_for_choice = false
 	show_dialog_button_indicator(false)
 	var _d : DialogItem = dialog_items[dialog_item_index]
-	set_dialog_data(_d)
 	
+	if _d is DialogText:
+		set_dialog_text(_d as DialogText)
+	elif _d is DialogChoice:
+		set_dialog_choice(_d as DialogChoice)
+	
+	pass
+
+func set_dialog_text(_d : DialogText) -> void:
+	content.text = _d.text
+	name_label.text = _d.npc_info.NPC_name
+	portrait_sprite.texture = _d.npc_info.portrait
+	portrait_sprite.audio_pitch_base = _d.npc_info.dialogue_audio_pitch
 	content.visible_characters = 0
 	text_length = content.get_total_character_count()
 	plain_text = content.get_parsed_text()
 	text_in_progress = true
 	start_timer()
+	pass
+
+## set Dialog choice UI based on parameters
+func set_dialog_choice(_d : DialogChoice) -> void:
+	choice_options.show()
+	waitin_for_choice = true
+	for c in choice_options.get_children():
+		c.queue_free()
+	for i in _d.dialog_branches.size():
+		var _new_choice : Button = Button.new()
+		_new_choice.text = _d.dialog_branches[i].text
+		_new_choice.alignment = HORIZONTAL_ALIGNMENT_LEFT
+		_new_choice.pressed.connect(_dialog_choice_selected.bind(_d.dialog_branches[i]))
+		choice_options.add_child(_new_choice)
+	await get_tree().process_frame
+	choice_options.get_child(0).grab_focus()
+	pass
+
+func _dialog_choice_selected(_d : DialogBranch) -> void:
+	choice_options.hide()
+	show_dialog(_d.dialog_items)
+	
 	pass
 
 func _on_timer_timeout() -> void:
@@ -97,13 +136,7 @@ func _on_timer_timeout() -> void:
 	pass
 	
 	
-func set_dialog_data(_d : DialogItem) -> void:
-	if _d is DialogText:
-		content.text = _d.text
-	name_label.text = _d.npc_info.NPC_name
-	portrait_sprite.texture = _d.npc_info.portrait
-	portrait_sprite.audio_pitch_base = _d.npc_info.dialogue_audio_pitch
-	pass
+
 
 func show_dialog_button_indicator(_is_visible: bool) -> void:
 	dialog_progress_indicator.visible = _is_visible
---
+++ Interactables/Dialog/Scripts/dialog_branch.gd
@@ -0,0 +1,16 @@
+@tool
+@icon("res://GUI/Dialog_system/icons/answer_bubble.svg")
+class_name DialogBranch extends DialogItem
+
+
+@export var text : String = "ok ..."
+
+var dialog_items : Array[DialogItem]
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	for c in get_children():
+		if c is DialogItem:
+			dialog_items.append(c)
