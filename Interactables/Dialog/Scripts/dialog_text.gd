6615eb8dbac25253bba5012b4ca2777b2b36f5b9
Pascal-Casier <pascal.casier1973@gmail.com>
2026-01-24 20:35:42 -0300

    E36 done dialog in editor

--- Interactables/Dialog/Scripts/dialog_text.gd
+++ Interactables/Dialog/Scripts/dialog_text.gd
@@ -2,4 +2,13 @@
 @icon("res://GUI/Dialog_system/icons/text_bubble.svg")
 class_name DialogText extends DialogItem
 
-@export_multiline var text : String = "Placeholder text"
+@export_multiline var text : String = "Placeholder text" : set = _set_text
+func _set_text(value : String) -> void:
+	text = value
+	if Engine.is_editor_hint():
+		if example_dialog:
+			_set_editor_display()
+
+func _set_editor_display() -> void:
+	example_dialog.set_dialog_text(self)
+	example_dialog.content.visible_characters = -1
